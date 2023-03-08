1246fb3e3d86b8bdd8154a29033c52498e90ec45
Julian Gercke <julian.gercke@rnd.de>
2023-03-08 10:00:11 +0100

    We dont have download statistics for app versions from the apple store

--- models/intermediate/app_version_report/int_apple_store__app_version.sql
+++ models/intermediate/app_version_report/int_apple_store__app_version.sql
@@ -11,9 +11,9 @@ subsetted as (
         'apple_store' as app_platform,
         app_name, 
         app_version,
-        sum(first_time_downloads) as first_time_downloads,
-        sum(redownloads) as redownloads,
-        sum(total_downloads) as total_downloads,
+        CAST(NULL as int64) as first_time_downloads,
+        CAST(NULL as int64) as redownloads,
+        CAST(NULL as int64) as total_downloads,
         sum(deletions) as deletions,
         sum(crashes) as crashes
     from app_version_report
