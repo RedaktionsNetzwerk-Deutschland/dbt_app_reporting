3ac3b777e3cf1051df70ce4ce4deb7aa7caa8bc9
Julian Gercke <julian.gercke@rnd.de>
2023-04-06 15:43:02 +0200

    using uninstall_events as deletions for google_play

--- models/intermediate/overview_report/int_google_play__overview.sql
+++ models/intermediate/overview_report/int_google_play__overview.sql
@@ -13,7 +13,7 @@ adapter as (
         device_installs as first_time_downloads,
         install_events - device_installs as redownloads,
         install_events as total_downloads,
-        device_uninstalls as deletions,
+        uninstall_events as deletions,
         store_listing_visitors as page_views,
         store_listing_acquisitions as user_acquisitions,
         crashes
